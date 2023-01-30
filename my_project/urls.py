6801300a7dad829a53ba7dcbd9e21d3b87db8ef7
LMK <icgirl2000@gmail.com>
2023-01-31 00:47:25 +0900

    3주차 django 개발

--- my_project/urls.py
+++ my_project/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('mysite/', include('mysite.urls')),
     path('', include('pages.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import MainContent
 
 def index(request) :
     content_list = MainContent.objects.order_by('-pub_date')
     context = {'content_list': content_list}
     return render(request, 'mysite/content_list.html', context)
+
+def detail(request, content_id):
+    content_list = get_object_or_404(MainContent, pk=content_id)
+    context = {'content_list': content_list}
+    return render(request, 'mysite/content_detail.html', context)
